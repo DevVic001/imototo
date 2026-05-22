224348d64cbf7a886b1bff08a388dcc2829545c6
DevVic001 <victoremmanuel1876@gmail.com>
2026-05-22 17:23:41 +0100

    ok

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,7 @@ const allowedOrigins = [
   'https://www.imototocleanings.co.uk',
   'http://localhost:5174',
   'http://127.0.0.1:5174',
+  'https://admin.dashboard.imototocleanings.co.uk',
 ];
 
 if (process.env.ADMIN_ALLOWED_ORIGINS) {
